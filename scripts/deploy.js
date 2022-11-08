adeb53ca427cda2a2b8974aaa55151da9a41f533
shreyas0924 <shreyasp0924@gmail.com>
2022-11-08 19:43:36 +0530

    verify function

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,6 @@
 //importss
 
-const { ethers } = require("hardhat")
+const { ethers, run } = require("hardhat")
 
 //async function
 
@@ -12,8 +12,20 @@ const main = async () => {
   console.log(`Deployed contract to ${simpleStorage.address}`)
 }
 
-async function verify(contractAddrss, args) {
-
+async function verify(contractAddrss, args) { //verifying contract
+  console.log("Verfiying contract....")
+  try {
+    await run("verify:verify", {
+      address: contractAddrss,
+      constructorArguments: args,
+    })
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified")
+    } else {
+      console.log(e)
+    }
+  }
 }
 
 main()
