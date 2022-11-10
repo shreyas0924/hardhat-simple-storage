d0d88adacdcce21d7559a52b5b43cbe223febc8f
shreyas0924 <shreyasp0924@gmail.com>
2022-11-10 21:54:04 +0530

    added verification process

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,6 @@
 //importss
 
-const { ethers, run } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 //async function
 
@@ -10,9 +10,16 @@ const main = async () => {
   const simpleStorage = await SimpleStorageFactory.deploy()
   await simpleStorage.deployed()
   console.log(`Deployed contract to ${simpleStorage.address}`)
+  console.log(network.config)
+
+  if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6) // wait for 6 blocks then run the verification process
+    await verify(simpleStorage.address , []) // empty arguments
+  }
 }
 
-async function verify(contractAddrss, args) { //verifying contract
+async function verify(contractAddrss, args) {
+  //verifying contract
   console.log("Verfiying contract....")
   try {
     await run("verify:verify", {
