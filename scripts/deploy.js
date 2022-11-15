238af2c6ddfcd640b33480c5d6c9e4fcea51455f
shreyas0924 <shreyasp0924@gmail.com>
2022-11-15 20:01:15 +0530

    added comments

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,28 +5,34 @@ const { ethers, run, network } = require("hardhat")
 //async function
 
 const main = async () => {
+  //Deplooy {
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
   console.log("Deploying....")
   const simpleStorage = await SimpleStorageFactory.deploy()
   await simpleStorage.deployed()
   console.log(`Deployed contract to ${simpleStorage.address}`)
-  console.log(network.config)
+  
+  //}
 
+  //Verfiy{
   if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
     await simpleStorage.deployTransaction.wait(6) // wait for 6 blocks then run the verification process
     await verify(simpleStorage.address, []) // empty arguments
   }
+  //}
 
-  const currentValue = await simpleStorage.retrive()
+  const currentValue = await simpleStorage.retrieve()
   console.log(`Current value is ${currentValue}`)
 
   //Update the current value
   const transactionResponse = await simpleStorage.store(7)
   await transactionResponse.wait(1)
-  const updatedValue = await simpleStorage.retrive()
+  const updatedValue = await simpleStorage.retrieve()
   console.log(`Updated value is ${updatedValue}`)
 }
 
+
+//verify functioon
 async function verify(contractAddrss, args) {
   //verifying contract
   console.log("Verfiying contract....")
