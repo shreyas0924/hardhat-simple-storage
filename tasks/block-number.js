216546b88cfcb2c9782448456bccb8da9fce6235
shreyas0924 <shreyasp0924@gmail.com>
2022-11-16 19:47:58 +0530

    testing locally in a js vm testnet using node

--- tasks/block-number.js
+++ tasks/block-number.js
@@ -1,6 +1,6 @@
 const { task } = require("hardhat/config")
 
-task("blcok-number", "Prints the current block number").setAction(
+task("block-number", "Prints the current block number").setAction(
   async (taskargs, hre) => {
     // hre is hardhat runtime environment
     const blockNumber = await hre.ethers.provider.getBlockNumber()
